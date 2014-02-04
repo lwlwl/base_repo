5eab718a31fe145dd0534dfe3e9b1524af5c07e3
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2014-02-04 13:15:30 +0400

    change

--- module/src/Main.java
+++ module/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello");
     }
 
     private static class Inner {
-        public static final int CONST1 = 1;
+        public static final int CONST_1 = 1;
+        public static final int CONST_2 = 2;
     }
 }
