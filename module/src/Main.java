5769b1e4538cb920fa4eaf78d3f8197220aefd31
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2014-02-11 16:54:31 +0400

    1

--- module/src/Main.java
+++ module/src/Main.java
@@ -7,4 +7,16 @@ public class Main {
         public static final int CONST_1 = 1;
         public static final int CONST_2 = 2;
     }
+
+    public static void f1() {
+        System.out.println("Hello");
+        System.out.println("This");
+        System.out.println("F1");
+    }
+
+    public static void f2() {
+        System.out.println("Hello");
+        System.out.println("This");
+        System.out.println("F2");
+    }
 }
