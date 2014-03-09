bcfe2d68ad9849add376ae2885043c2ecaa3d5d1
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2014-03-09 17:03:30 +0400

    1

--- module/src/Main.java
+++ module/src/Main.java
@@ -1,3 +1,11 @@
 public class Main {
-    // asdasdasd;
+    String awdse;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public void foo() {
+        return;
+    }
 }
