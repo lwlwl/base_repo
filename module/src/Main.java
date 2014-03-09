5116d85029d2183de36a4bb0c34406217f940898
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2014-03-09 17:02:36 +0400

    asd

--- module/src/Main.java
+++ module/src/Main.java
@@ -1,19 +1,2 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-
-    private static class Inner {
-        public static final int CONST_1 = 1;
-        public static final int CONST_2 = 2;
-    }
-
-    public static void f2() {
-        System.out.println("Hello");
-        System.out.println("This");
-        System.out.println("F2");
-    }
-
-    public static void f1() {
-    }
 }
