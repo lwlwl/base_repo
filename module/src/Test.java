d582c0d79b86747245798b43c161092bdae0d39d
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2014-06-16 12:14:47 +0400

    1

---
+++ module/src/Test.java
@@ -0,0 +1,23 @@
+/**
+ * some
+ * comment
+ */
+
+public class Test {
+    public enum MyEnum {A, B, C}
+
+    public static MyEnum A() {
+        int i;
+        return MyEnum.A;
+    }
+
+    public static MyEnum B() {
+        int i;
+        return MyEnum.B;
+    }
+
+    public static MyEnum C() {
+        int i;
+        return MyEnum.C;
+    }
+}
