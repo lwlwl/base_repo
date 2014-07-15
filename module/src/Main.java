afa5cb0d64e6c3ec336d617f26d86d5a3457854a
Aleksey Pivovarov <Aleksey.Pivovarov@jetbrains.com>
2014-07-15 20:04:31 +0400

    Merge branch 'php-merge-a' into php-merge-b
    Conflicts:
    	example.php

--- module/src/Main.java
+++ module/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     String awdse;
 
     public static void main(String[] args) {
-        System.out.println();
+        if (true) {
+            System.out.println();
+        }
     }
 
     public void foo() {
