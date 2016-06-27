7403355eee9ca00c2435b2832b80f5e66321d2dd
Aleksey Pivovarov <AMPivovarov@gmail.com>
2016-06-27 19:31:39 +0300

    Ч

--- module/src/Main.java
+++ module/src/Main.java
@@ -1,15 +1,10 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
-	String awdse;
+
+
 
 	public static void main(String[] args) {
-		if (true) {
-			System.out.println("Hello world");
-		} else {
-			return;
-		}
-	}
-
-	public void foo() {
-		return;
 	}
 }
