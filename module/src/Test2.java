0a1b87fd8321771d5859eac388b3f7aab605b714
Aleksey Pivovarov <AMPivovarov@gmail.com>
2016-08-02 12:20:43 +0300

    WWW

---
+++ module/src/Test2.java
@@ -0,0 +1,13 @@
+public class Test2 {
+    public static class A {
+    }
+
+    public static class B extends A {
+    }
+
+    public static class C extends A {
+    }
+
+    public static class D extends B {
+    }
+}
